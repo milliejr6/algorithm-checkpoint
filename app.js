4ec71c6da1a4697ae165511840a472e0ed8dbb8a
Millie <deletobi653@gmail.com>
2025-05-20 17:37:56 +0100

    mark-abeg

--- app.js
+++ app.js
@@ -1,39 +1,29 @@
-# Initialize counters
-length = 0
-word_count = 0
-vowel_count = 0
+let length = 0;
+let words = 0;
+let vowels = 0;
 
-# Define vowels
-vowels = "aeiouAEIOU"
+const vowelSet = ["a", "e", "i", "o", "u"];
 
-print("Enter a sentence that ends with a period (.)")
+let sentence = prompt("Enter a sentence ending with a period ('.'):");
 
-# Read the sentence character by character
-in_word = False
+let inWord = false;
 
-while True:
-    ch = input()
+for (let i = 0; i < sentence.length; i++) {
+  let ch = sentence[i];
+  length++;
 
-    if len(ch) != 1:
-        print("Please enter one character at a time.")
-        continue
+  if (vowelSet.includes(ch.toLowerCase())) {
+    vowels++;
+  }
 
-    length += 1
+  if (ch !== " " && ch !== "." && !inWord) {
+    words++;
+    inWord = true;
+  } else if (ch === "") {
+    inWord = false;
+  }
 
-    if ch in vowels:
-        vowel_count += 1
-
-    if ch == " ":
-        in_word = False
-    elif ch == ".":
-        if in_word:
-            word_count += 1
-        break
-    else:
-        if not in_word:
-            in_word = True
-
-# Output the results
-print("Length of the sentence (characters):", length)
-print("Number of words:", word_count)
-print("Number of vowels:", vowel_count)
+  if (ch === ".") {
+    break;
+  }
+}
